7ee92c51636b04a36348d1ea10784142e2d348ee
FoxxMD <FoxxMD@users.noreply.github.com>
2024-01-30 11:10:10 -0500

    refactor: Refactor PersistentClient as a class instead of function
    * Fixes unhandled timeout rejection on `connect`
    * Allows initiating `connect` separately from instantiation using `await persistentClient.connect()`
      * `connect()` is still exported at top-level to prevent breaking change
    * Better reconnection logic
    * is an `EventEmitter` that re-emits `castv2` events
    * exposes `castv2` client, `connected` and `shouldReconnect` fields

--- examples/platform.ts
+++ examples/platform.ts
@@ -1,6 +1,7 @@
-import {createPlatform, connect} from '../src'
+import {createPlatform, PersistentClient} from '../src'
 ;(async () => {
-  const client = await connect({host: '192.168.1.150'})
+  const client = new PersistentClient({host: '192.168.1.150'})
+  await client.connect()
   const platform = await createPlatform(client)
   const status = await platform.getStatus()
   console.log('current status', status)
